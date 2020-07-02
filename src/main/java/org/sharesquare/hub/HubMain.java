2a4779408b7a837e6052adf94f2964468debe95d
Katrin Ka <hellomynameiskatrin@gmail.com>
2020-07-02 15:24:48 +0200

    not used

--- src/main/java/org/sharesquare/hub/HubMain.java
+++ src/main/java/org/sharesquare/hub/HubMain.java
@@ -1,16 +1,13 @@
 package org.sharesquare.hub;
 
 
-import org.sharesquare.hub.configuration.SystemConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @ComponentScan({"org.sharesquare"})
-@EnableConfigurationProperties(SystemConfiguration.class)
 @EnableJpaRepositories("org.sharesquare.hub.repository")
 public class HubMain {
 
