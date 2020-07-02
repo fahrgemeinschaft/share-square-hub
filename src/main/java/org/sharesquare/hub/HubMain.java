e63418ea49a61ff39cc8cf8e631081446594d87c
Katrin Ka <hellomynameiskatrin@gmail.com>
2020-07-02 15:24:03 +0200

    use jpa repository

--- src/main/java/org/sharesquare/hub/HubMain.java
+++ src/main/java/org/sharesquare/hub/HubMain.java
@@ -6,14 +6,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @ComponentScan({"org.sharesquare"})
 @EnableConfigurationProperties(SystemConfiguration.class)
+@EnableJpaRepositories("org.sharesquare.hub.repository")
 public class HubMain {
 
     public static void main(String[] args) {
         SpringApplication.run(HubMain.class, args);
     }
-
 }
