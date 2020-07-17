af23d50182950d1995566eae7262013214ae3d53
Katrin Ka <hellomynameiskatrin@gmail.com>
2020-07-17 19:06:29 +0200

    example data option works with database, sanitizer not needed

--- src/main/java/org/sharesquare/hub/configuration/ExampleData.java
+++ src/main/java/org/sharesquare/hub/configuration/ExampleData.java
@@ -2,12 +2,12 @@ package org.sharesquare.hub.configuration;
 
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
-import org.sharesquare.model.Connector;
-import org.sharesquare.model.TargetSystem;
-import org.sharesquare.repository.IRepository;
-import org.sharesquare.repository.SimpleInMemoryRepository;
+import org.sharesquare.hub.model.data.EntityConnector;
+import org.sharesquare.hub.model.data.EntityTargetSystem;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -18,52 +18,57 @@ public final class ExampleData {
 	private ExampleData() {
 	}
 
-	protected static IRepository<Connector> connectorRepo() {
-		IRepository<Connector> connectorRepo = new SimpleInMemoryRepository<Connector>();
+	protected static List<EntityTargetSystem> getEntityTargetSystems() {
+		List<EntityTargetSystem> exampleTargetSystems = new ArrayList<>();
 
 		try {
-			TargetSystem exampleTargetSystem = new TargetSystem();
-			exampleTargetSystem.setId(UUID.randomUUID());
+			EntityTargetSystem exampleTargetSystem = new EntityTargetSystem();
+			exampleTargetSystem.setId(UUID.fromString("78bb139f-30ce-4e64-b300-015a38954140"));
 			exampleTargetSystem.setName("Fahrgemeinschaft.de");
 			exampleTargetSystem.setDescription("Kostenlose Mitfahrgelegenheit");
-
 			exampleTargetSystem.setVanityUrl(new URL("http://www.fahrgemeinschaft.de"));
+			exampleTargetSystem.setContentLanguage("de");
+			exampleTargetSystem.setDataProtectionRegulations("(hier Text einf\u00fcgen)");
 
-			Connector exampleConnector = new Connector();
-			//exampleConnector.setTargetSystem(exampleTargetSystem);
+			EntityConnector exampleConnector = new EntityConnector();
 			exampleConnector.setAliveCheckWebhook(new URL("https://www.fahrgemeinschaft.de/api/alivecheck"));
 			exampleConnector.setOfferUpdateWebhook(new URL("https://www.fahrgemeinschaft.de/api/offers"));
+			exampleTargetSystem.setConnector(exampleConnector);
 
-			connectorRepo.create(exampleConnector);
+			exampleTargetSystems.add(exampleTargetSystem);
 
-			exampleTargetSystem = new TargetSystem();
-			exampleTargetSystem.setId(UUID.randomUUID());
+			exampleTargetSystem = new EntityTargetSystem();
+			exampleTargetSystem.setId(UUID.fromString("c38eea72-f5c1-4087-b099-acd99969471c"));
 			exampleTargetSystem.setName("ride2Go");
 			exampleTargetSystem.setDescription("Fahrgemeinschaften und Mitfahrgelegenheiten");
 			exampleTargetSystem.setVanityUrl(new URL("http://www.ride2go.com"));
+			exampleTargetSystem.setContentLanguage("de");
+			exampleTargetSystem.setDataProtectionRegulations("(hier Text einf\u00fcgen)");
 
-			exampleConnector = new Connector();
-			//exampleConnector.setTargetSystem(exampleTargetSystem);
+			exampleConnector = new EntityConnector();
 			exampleConnector.setAliveCheckWebhook(new URL("https://www.ride2go.com/api/alivecheck"));
 			exampleConnector.setOfferUpdateWebhook(new URL("https://www.ride2go.com/api/offers"));
+			exampleTargetSystem.setConnector(exampleConnector);
 
-			connectorRepo.create(exampleConnector);
+			exampleTargetSystems.add(exampleTargetSystem);
 
-			exampleTargetSystem = new TargetSystem();
-			exampleTargetSystem.setId(UUID.randomUUID());
+			exampleTargetSystem = new EntityTargetSystem();
+			exampleTargetSystem.setId(UUID.fromString("cabd13ae-d074-4845-a1dd-fa8cb32177c6"));
 			exampleTargetSystem.setName("Mitfahrzentrale MiFaZ");
 			exampleTargetSystem.setDescription("Mitfahrgelegenheit und Fahrgemeinschaft");
 			exampleTargetSystem.setVanityUrl(new URL("http://www.mifaz.de"));
+			exampleTargetSystem.setContentLanguage("de");
+			exampleTargetSystem.setDataProtectionRegulations("(hier Text einf\u00fcgen)");
 
-			exampleConnector = new Connector();
-			//exampleConnector.setTargetSystem(exampleTargetSystem);
+			exampleConnector = new EntityConnector();
 			exampleConnector.setAliveCheckWebhook(new URL("https://www.mifaz.de/api/alivecheck"));
 			exampleConnector.setOfferUpdateWebhook(new URL("https://www.mifaz.de/api/offers"));
+			exampleTargetSystem.setConnector(exampleConnector);
 
-			connectorRepo.create(exampleConnector);
+			exampleTargetSystems.add(exampleTargetSystem);
 		} catch (MalformedURLException e) {
 			log.warn("Example data has malformed URL: " + e.getMessage());
 		}
-		return connectorRepo;
+		return exampleTargetSystems;
 	}
 }
