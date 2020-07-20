ea82e72996675763e9d4f364985d8c95d4493a67
Katrin Ka <hellomynameiskatrin@gmail.com>
2020-07-20 10:17:06 +0200

    format and remove ids

--- src/main/java/org/sharesquare/hub/configuration/ExampleData.java
+++ src/main/java/org/sharesquare/hub/configuration/ExampleData.java
@@ -22,47 +22,44 @@ public final class ExampleData {
 		List<EntityTargetSystem> exampleTargetSystems = new ArrayList<>();
 
 		try {
+			EntityConnector exampleConnector = new EntityConnector();
+			exampleConnector.setAliveCheckWebhook(new URL("https://www.fahrgemeinschaft.de/api/alivecheck"));
+			exampleConnector.setOfferUpdateWebhook(new URL("https://www.fahrgemeinschaft.de/api/offers"));
+
 			EntityTargetSystem exampleTargetSystem = new EntityTargetSystem();
-			exampleTargetSystem.setId(UUID.fromString("78bb139f-30ce-4e64-b300-015a38954140"));
 			exampleTargetSystem.setName("Fahrgemeinschaft.de");
 			exampleTargetSystem.setDescription("Kostenlose Mitfahrgelegenheit");
 			exampleTargetSystem.setVanityUrl(new URL("http://www.fahrgemeinschaft.de"));
 			exampleTargetSystem.setContentLanguage("de");
 			exampleTargetSystem.setDataProtectionRegulations("(hier Text einf\u00fcgen)");
-
-			EntityConnector exampleConnector = new EntityConnector();
-			exampleConnector.setAliveCheckWebhook(new URL("https://www.fahrgemeinschaft.de/api/alivecheck"));
-			exampleConnector.setOfferUpdateWebhook(new URL("https://www.fahrgemeinschaft.de/api/offers"));
 			exampleTargetSystem.setConnector(exampleConnector);
 
 			exampleTargetSystems.add(exampleTargetSystem);
 
+			exampleConnector = new EntityConnector();
+			exampleConnector.setAliveCheckWebhook(new URL("https://www.ride2go.com/api/alivecheck"));
+			exampleConnector.setOfferUpdateWebhook(new URL("https://www.ride2go.com/api/offers"));
+
 			exampleTargetSystem = new EntityTargetSystem();
-			exampleTargetSystem.setId(UUID.fromString("c38eea72-f5c1-4087-b099-acd99969471c"));
 			exampleTargetSystem.setName("ride2Go");
 			exampleTargetSystem.setDescription("Fahrgemeinschaften und Mitfahrgelegenheiten");
 			exampleTargetSystem.setVanityUrl(new URL("http://www.ride2go.com"));
 			exampleTargetSystem.setContentLanguage("de");
 			exampleTargetSystem.setDataProtectionRegulations("(hier Text einf\u00fcgen)");
-
-			exampleConnector = new EntityConnector();
-			exampleConnector.setAliveCheckWebhook(new URL("https://www.ride2go.com/api/alivecheck"));
-			exampleConnector.setOfferUpdateWebhook(new URL("https://www.ride2go.com/api/offers"));
 			exampleTargetSystem.setConnector(exampleConnector);
 
 			exampleTargetSystems.add(exampleTargetSystem);
 
+			exampleConnector = new EntityConnector();
+			exampleConnector.setAliveCheckWebhook(new URL("https://www.mifaz.de/api/alivecheck"));
+			exampleConnector.setOfferUpdateWebhook(new URL("https://www.mifaz.de/api/offers"));
+
 			exampleTargetSystem = new EntityTargetSystem();
-			exampleTargetSystem.setId(UUID.fromString("cabd13ae-d074-4845-a1dd-fa8cb32177c6"));
 			exampleTargetSystem.setName("Mitfahrzentrale MiFaZ");
 			exampleTargetSystem.setDescription("Mitfahrgelegenheit und Fahrgemeinschaft");
 			exampleTargetSystem.setVanityUrl(new URL("http://www.mifaz.de"));
 			exampleTargetSystem.setContentLanguage("de");
 			exampleTargetSystem.setDataProtectionRegulations("(hier Text einf\u00fcgen)");
-
-			exampleConnector = new EntityConnector();
-			exampleConnector.setAliveCheckWebhook(new URL("https://www.mifaz.de/api/alivecheck"));
-			exampleConnector.setOfferUpdateWebhook(new URL("https://www.mifaz.de/api/offers"));
 			exampleTargetSystem.setConnector(exampleConnector);
 
 			exampleTargetSystems.add(exampleTargetSystem);
