27685cafabe9aad90b3412baa2ea8dc6aded1fd2
Katrin Ka <hellomynameiskatrin@gmail.com>
2020-08-10 13:54:48 +0200

    manage target systems only for this client

--- src/main/java/org/sharesquare/hub/repository/TargetSystemRepository.java
+++ src/main/java/org/sharesquare/hub/repository/TargetSystemRepository.java
@@ -1,9 +1,12 @@
 package org.sharesquare.hub.repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 import org.sharesquare.hub.model.data.EntityTargetSystem;
 import org.springframework.data.repository.CrudRepository;
 
 public interface TargetSystemRepository extends CrudRepository<EntityTargetSystem, UUID> {
+
+	Optional<EntityTargetSystem> findByIdAndClientId(UUID id, String clientId);
 }
